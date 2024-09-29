9c49db8ed7757ac67647f2f2064e811405d0273b
Allan Baptista <allan@beanimus.com>
2024-09-29 00:48:41 -0300

    finished all endpoints

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,15 @@
+import fastifyPlugin from "fastify-plugin";
+
 import stories from './stories.js';
 import users from './users.js';
 import votes from './votes.js';
 
 export { stories, users, votes };
 
-export const plugin = async app => {
+export const plugin = fastifyPlugin(async app => {
   await Promise.all([
     stories(app),
     users(app),
     votes(app),
   ]);
-}
+});
