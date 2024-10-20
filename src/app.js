a820e625105897eac78c066a8674eaba0609cb2f
Allan Baptista <allan@beanimus.com>
2024-10-20 10:35:39 -0300

    adding swagger & swagger ui

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import Fastify from "fastify";
+import swaggerPlugin from "@fastify/swagger";
+import swaggerUiPlugin from "@fastify/swagger-ui";
 import { plugin as dbPlugin } from "./db/index.js";
 import { plugin as repositoriesPlugin } from "./repositories/index.js";
 import { plugin as routesPlugin } from "./routes/index.js";
@@ -6,6 +8,8 @@ import { plugin as routesPlugin } from "./routes/index.js";
 export default function init () {
   const app = Fastify({ logger: true });
 
+  app.register(swaggerPlugin);
+  app.register(swaggerUiPlugin);
   app.register(dbPlugin);
   app.register(repositoriesPlugin);
   app.register(routesPlugin);
--- src/routes/healthchecks.js
+++ src/routes/healthchecks.js
@@ -1,8 +1,28 @@
+const healthRoutesSchema = {
+  schema: {
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          status: { type: 'string' },
+        },
+      },
+      503: {
+        type: 'object',
+        properties: {
+          status: { type: 'string' },
+          message: { type: 'string' },
+        },
+      },
+    },
+  }
+};
+
 /**
  * @param {import('fastify').FastifyInstance} app
  */
 export default async app => {
-  app.get('/ready', async (_, reply) => {
+  app.get('/ready', healthRoutesSchema, async (_, reply) => {
     const results = await Promise.all([
       app.repositories.stories.ready(),
       app.repositories.users.ready(),
@@ -16,7 +36,7 @@ export default async app => {
     return reply.send({ status: 'ok' });
   });
 
-  app.get('/alive', async (_, reply) => {
+  app.get('/alive', healthRoutesSchema, async (_, reply) => {
     await app.db.execute('SELECT 1');
 
     return reply.send({ status: 'ok' });
